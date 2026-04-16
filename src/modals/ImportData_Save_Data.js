c2eb4c17929b1a2d6a8aaf0d1558d56b31e33b55
Nathan Lee <nathanleeahern@gmail.com>
2026-04-16 17:45:15 +1000

    Finished implementing modal component with one set of JSX and a useEffect to dynamically assign its content based on the saveModalState obj. Testing failed. Need to debug.

--- src/modals/ImportData_Save_Data.js
+++ src/modals/ImportData_Save_Data.js
@@ -1,11 +1,11 @@
-import styled from 'styled-components';
-import { Modal, ModalBody, ModalHeader,ModalFooter, UncontrolledCollapse} from 'reactstrap';
+import { useEffect, useState } from 'react';
+import { Button, Modal, ModalBody, ModalHeader,ModalFooter, UncontrolledCollapse} from 'reactstrap';
 
 /* 
     The state object defined in the parent (ImportData) and passed into this component contains the following properties:
     
     const[saveModalState, setSaveModalState] = useState({
-        responseErr : false,  // if T, opens a custom error modal in the event that !response.ok ('Try Again'/'Cancel')
+        responseErr : "",  // if truthy (an error message has been assigned) which will open a custom error modal('Try Again'/'Cancel')
         newSubmit : true,     // this valus is used to open either a confirm modal ('Confirm'/'Cancel') or a ('Delete'/'Overwrite') modal 
         openModal: false,     // this value controls whether the modal is open or closed  
     });
@@ -17,11 +17,58 @@ import { Modal, ModalBody, ModalHeader,ModalFooter, UncontrolledCollapse} from '
 export default function SaveModal({
     saveModalState, setSaveModalState
 }){
+
+    //local state variable which will store the modal body string
+    const [header, setHeader] = useState("");
+    const [body, setBody] = useState("");
+    const [successButton, setSuccessButton] = useState("");
+    const [dangerButton, setDangerButton] = useState("");
+
+    const ERROR_HEADER = "ERROR";
+    const REGULAR_HEADER = "Confirm Action";
+    
+    const CONFIRM_BODY = "Confirm Action: Saving Transactions to database";
+    const DEL_OVERWRITE_BODY = "You have previously saved transactions this month. Would you like to add to existing transactions or overwrite them?"
+    const ERROR_BODY = "An error occurred saving data.";
+
+    const ERR_SUCCESS = "Try Again";
+    const CANCEL = "Cancel";
+    const CONFIRM = "Confirm";
+    const OVERWRITE = "Overwrite";
+    const ADD_TO = "Add to";
+
     // toggle function which is called to open/close modal
     const saveDataToggle = () => setSaveModalState((prevState) => ({
         ...prevState, //preserve the other object properties
         openModal : !prevState.openModal // reverse the openModal bool to open/close modal
     }));
+
+    //component useEffect hook function with cleanup
+    //function will run everytime the SaveModal component is called to be rendered, and also whenever saveModalState object changes
+    //the side effect it will perform is to dynamically assign modal content based on the saveModalState(e.g. Modal Header, Body, Buttons)
+    useEffect(() => {
+        if(saveModalState.responseErr){
+            setHeader(ERROR_HEADER);
+            setBody(ERROR_BODY);
+            setSuccessButton(ERR_SUCCESS);
+            setDangerButton(CANCEL);
+
+        } else{
+            if(saveModalState.newSubmit){
+                setHeader(REGULAR_HEADER);
+                setBody(CONFIRM_BODY);
+                setSuccessButton(CONFIRM);
+                setDangerButton(CANCEL);
+            } else{
+                setHeader(REGULAR_HEADER);
+                setBody(DEL_OVERWRITE_BODY);
+                setSuccessButton(ADD_TO);
+                setDangerButton(OVERWRITE);
+            }
+
+        }
+        
+    }, [saveModalState]);
 
 
 
@@ -31,8 +78,45 @@ export default function SaveModal({
     //Alternatively, it may be more efficient to return a single set of JSX and conditionally render the Buttons and <p> message using embedded JS
     
     return(
-        <>
-        </>
+        <div> 
+          <Modal isOpen={saveModalState.openModal} toggle={saveDataToggle}>
+            <ModalHeader toggle={saveDataToggle}>{header}</ModalHeader>
+                <ModalBody>
+                    {body}
+                    <br />
+                    {
+                        saveModalState.responseErr && (
+                            <em>{saveModalState.responseErr}</em>
+                        )
+                    }
+                </ModalBody>
+            <ModalFooter>
+              <Button color="success" onClick={(e) => {
+
+                //stop the dropdown and submenu from closing
+                e.preventDefault(); 
+                e.stopPropagation(); 
+
+                }}
+              >
+                {successButton}
+              </Button>
+              <Button color="danger" onClick={(e) => {
+
+                //stop the dropdown and submenu from closing
+                e.preventDefault(); 
+                e.stopPropagation(); 
+
+                // reset state
+                
+            
+              }}
+              >
+                {dangerButton}
+              </Button>
+            </ModalFooter>
+          </Modal>
+        </div>
 
     );
 
